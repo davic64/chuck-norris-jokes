9a1ce6f8258a9b2fc0a5c03268f67b31899a0b91
David Victoria <davidvictoria64@gmail.com>
2021-12-20 21:33:45 -0600

    :sparkles: FormCategories component finished

--- src/Components/FormCategories.js
+++ src/Components/FormCategories.js
@@ -1,33 +1,49 @@
+/* eslint-disable object-curly-newline */
 import React, { useContext, useEffect, useState } from 'react';
 
 // Chakra UI
-import { Button, FormControl, Select } from '@chakra-ui/react';
+import { Button, Center, FormControl, Select } from '@chakra-ui/react';
 
 // Context
 import ChuckContext from '../context/chuknorris/chuckContext';
 
 const FormCategories = () => {
     const chuckContext = useContext(ChuckContext);
-    const { categories, getCategories } = chuckContext;
+    const { categories, getCategories, getJoke } = chuckContext;
+
+    const [category, setCategory] = useState('');
 
     useEffect(() => {
         getCategories();
     }, []);
 
+    const categorySelected = (event) => {
+        setCategory(event.target.value);
+    };
+
+    const handle = (event) => {
+        event.preventDefault();
+        getJoke(category);
+    };
+
     return (
-        <>
-            <FormControl mt={8}>
-                <Select placeholder="all">
+        <form onSubmit={handle} style={{ width: '100%' }}>
+            <FormControl mt={8} w="100%">
+                <Select placeholder="random" onChange={categorySelected}>
                     {categories &&
-                        categories.map((category) => (
-                            <option value={category}>{category}</option>
+                        categories.map((categoryName) => (
+                            <option key={categoryName} value={categoryName}>
+                                {categoryName}
+                            </option>
                         ))}
                 </Select>
             </FormControl>
-            <Button colorScheme="teal" size="lg" mt={8}>
-                Generate
-            </Button>
-        </>
+            <Center>
+                <Button type="submit" colorScheme="teal" size="lg" mt={8}>
+                    Generate
+                </Button>
+            </Center>
+        </form>
     );
 };
 
